294ffc0849cb54f0e0d7296731c2165740c9a669
Stepan-Muller <mullerstepan4@gmail.com>
2024-06-04 22:07:37 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#7A9D8C",
+      secondary: "#ABA8BF",
+      text: "#050706"
+    },
+  },
+  plugins: [],
+}
